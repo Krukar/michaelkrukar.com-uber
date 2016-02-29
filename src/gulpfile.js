1567cf938fc795aef326fa7fbb25918009dae560
Michael Krukar <krukar@gmail.com>
2016-02-28 21:29:36 -0500

    Added js to gulp and generated minified single js file for online testing

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass');
+sass = require('gulp-sass'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+rename = require('gulp-rename');
 
 gulp.task('sass', function () {
 	return gulp.src('./scss/style.scss')
@@ -10,6 +13,29 @@ gulp.task('sass', function () {
 	.pipe(gulp.dest('../'));
 });
 
-gulp.task('default', ['sass'], function() {
+gulp.task('concat', function() {
+	return gulp.src(['node_modules/jquery/dist/jquery.min.js',
+		'node_modules/snapsvg/dist/snap.svg-min.js',
+		'js/app.js'])
+	.pipe(concat('uber.js'))
+	.pipe(gulp.dest('js'));
+});
+
+gulp.task('uglify', ['concat'], function() {
+	return gulp.src('js/uber.js')
+	.pipe(uglify({
+		compress:{
+			drop_console: true
+		}
+	}))
+	.pipe(rename({
+		extname: '.min.js'
+	}))
+	.pipe(gulp.dest('../'));
+});
+
+gulp.task('default', ['sass', 'concat', 'uglify'], function() {
+	gulp.watch('js/**/*.js', ['concat']);
+	gulp.watch('js/tv.js', ['uglify']);
 	gulp.watch(['scss/**/*.scss', 'js/**/*.scss'], ['sass']);
 });
