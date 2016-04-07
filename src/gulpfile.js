5bfd608e990d71e0a3f4949255e5dca28e816cbc
Michael Krukar <krukar@gmail.com>
2016-04-06 22:02:51 -0400

    Temporary solution to preview on staging. Need to fix gulp build

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -11,6 +11,18 @@ gulp.task('css', function () {
 	.pipe(gulp.dest('../'));
 });
 
+gulp.task('concat', function(){
+	return gulp.src(['node_modules/jquery/dist/jquery.min.js',
+		'node_modules/snapsvg/dist/snap.svg-min.js',
+		'node_modules/angular/angular.min.js',
+		'js/app.js',
+		'js/ngMapFactory.js',
+		'js/ngMap.js',
+		'js/ngAnimator.js'])
+	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest('js'));
+});
+
 gulp.task('default', ['css'], function() {
 	gulp.watch(['scss/**/*.scss'], ['css']);
 });
